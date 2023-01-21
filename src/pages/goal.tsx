cde7dbec411dc3e971d6dbe6e07fe9f32edccfcf
IsmaelSTEM <113748633+TheCodingWizard0@users.noreply.github.com>
2023-01-21 17:43:40 -0500

    Claire's commit

--- src/pages/goal.tsx
+++ src/pages/goal.tsx
@@ -1,18 +1,35 @@
 import { useState } from "react"
 
 const GoalPage = () => {
-    const [title, setTtitle] = useState('')
+    const [title, setTitle] = useState('');
+    const [radio1, setRadio1] = useState('');
+    const [radio2, setRadio2] = useState('');
     return (
-        <div className="goal">
-            <h2>Enter your Goal</h2>
-            <form>
-                <input
-                    type="text"
-                    required
-                    value={title}
-                />
-                <button>Set Goal</button>
-            </form>
+        <div className="flex justify-center h-screen goal">
+            <div className="text-center">
+                <h1 className="text-5xl pt-5 pb-40">Enter your Goal</h1>
+                <form className="flex flex-col text-center">
+                    <input
+                        type="text"
+                        required
+                        value={title}
+                        onChange={(a) => setTitle(a.target.value)}
+                    />
+                    <input
+                        type="radio"
+                        required
+                        value={radio1}
+                        onChange={(b) => setRadio1(b.target.value)}
+                    />
+                    <input
+                        type="radio"
+                        required
+                        value={radio2}
+                        onChange={(c) => setRadio2(c.target.value)}
+                    />
+                    <button className="pt-40">Set Goal</button>
+                </form>
+            </div>
         </div>
         )
 }
