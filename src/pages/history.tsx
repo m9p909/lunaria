e62e417635914206c988b9b630a19ea77ebaa7ef
IsmaelSTEM <113748633+TheCodingWizard0@users.noreply.github.com>
2023-01-22 10:38:26 -0500

    Additional touches to Savings History

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -6,25 +6,33 @@ const HistoryPage = () => {
                 <div className="flex flex-row text-center">
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You met your goal of $100 last month!</div>
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You saved an average of $25 per week!</div>
-                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3"></div>
+                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">
+                        <img className="w-1/3 h-auto object-center" src="/assets/IMG_0883.GIF"></img>
+                    </div>
                 </div>
                 <h2 className="text-[#372D40] pb-5 px-10">Goal 2</h2>
                 <div className="flex flex-row text-center">
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You met your goal of $500 over the past 3 months!</div>
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You saved an average of $133 per month!</div>
-                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3"></div>
+                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">
+                    <img className="w-1/3 h-auto object-center" src="/assets/IMG_0883.GIF"></img>
+                    </div>
                 </div>
                 <h2 className="text-[#372D40] pb-5 px-10">Goal 3</h2>
                 <div className="flex flex-row text-center">
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You did not meet your goal of $1000 over the past 6 months!</div>
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You saved an average of $200 per month!</div>
-                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3"></div>
+                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">
+                    <img className="w-1/3 h-auto grayscale object-center" src="/assets/IMG_0883.GIF"></img>
+                    </div>
                 </div>                
                 <h2 className="text-[#372D40] pb-5 px-10">Goal 4</h2>
                 <div className="flex flex-row text-center">
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You did not meet your goal of $2000 over the past 12 months!</div>
                     <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">You saved an average of $100 per month!</div>
-                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3"></div>
+                    <div className="bg-[#E5FFF8] hover:bg-[#B18AF8] hover:text-white text-[#372D40] border-black border-2 justify-center pt-6 pb-6 m-6 w-1/3">
+                    <img className="w-1/3 h-auto grayscale object-center" src="/assets/IMG_0883.GIF"></img>
+                    </div>
                 </div>            </div>
     )
 }
