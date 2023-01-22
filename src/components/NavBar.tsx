2718f85b5456114e722fcaedfeaa72d956d6b8c9
Fay Rushwany <83249765+fayrush@users.noreply.github.com>
2023-01-22 01:40:37 -0500

    setting up some css

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,14 +3,14 @@ import NavButton from './NavButton';
 
 export default function NavBar() {
     return(
-        <aside className="bg-[#E5FFF8] w-1/6 h-screen flex flex-col px-3 py-4">
+        <aside className="bg-[#E5FFF8] w-1/6 h-screen flex flex-col py-4 border-2-bg">
             <button className="flex justify-center p-5">
-                <img className="h-20" src="https://www.studiofun.com/wp-content/uploads/2020/01/Big-Bird-Header-1024x536.png"/> 
+                <img className="h-20" src='.../public/assets/IMG_0872.PNG/'/> 
             </button>
-            <NavButton label="Main" redirect="/" icon="https://cdn-icons-png.flaticon.com/512/25/25694.png" />
-            <NavButton label="Goals" redirect="/" icon="https://cdn-icons-png.flaticon.com/512/25/25694.png" />
-            <NavButton label="History" redirect="/" icon="https://cdn-icons-png.flaticon.com/512/25/25694.png" />
-            <NavButton label="Settings" redirect="/" icon="https://cdn-icons-png.flaticon.com/512/25/25694.png" />
+            <NavButton label="Main" redirect="/"/>
+            <NavButton label="Goals" redirect="/"/>
+            <NavButton label="History" redirect="/"/>
+            <NavButton label="Settings" redirect="/"/>
         </aside>
     );
 }
--- src/components/goal.tsx
+++ src/components/goal.tsx
@@ -10,45 +10,45 @@ const GoalPage = () => {
     return (
         <div className="flex justify-center h-screen bg-white] goal w-full">
             <div className="text-center">
-                <h1 className="text-5xl pt-10 pb-20 text-[#372D40]">LET'S GET YOUR FIRST GOAL!</h1>
-                <form className="flex flex-col text-center">
-                    <h2 className="pb-5">ENTER YOUR SAVINGS GOAL: </h2>
+                <h1 className="text-5xl py-10 text-[#372D40]">LET'S GET YOUR FIRST GOAL!</h1>
+                <form className="flex flex-col text-left">
+                    <h2 className="pb-5">ENTER YOUR SAVINGS GOAL:</h2>
                     <input
-                        className="text-[#372D40] border-black border-2"
+                        className="text-[#372D40] border-black border-2 p-2"
                         type="number"
                         maxLength={10}
                         required
                         value={title}
                         onChange={(a) => setTitle(a.target.value)}
                     />
-                    <h2 className="pt-5">GOAL DURATION:</h2>
+                    <h2 className="py-5">GOAL DURATION:</h2>
                     <ToggleGroup.Root
                         className="flex"
                         type="single"
                         value={radio1}
-                        onValueChange={(radio1) => {
+                        onValueChange={(radio1: any) => {
                             if (radio1) setRadio1(radio1)
                         }}
                     >
-                        <ToggleGroup.Item className={`flex-grow p-6 rounded-l-lg mt-10 mb-10 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:opacity-70 ${radio1 === '3 months' ? "bg-[#b18af8] text-white" : "" }`} value="3 months">3 months</ToggleGroup.Item>
-                        <ToggleGroup.Item className={`flex-grow p-6 mt-10 mb-10 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio1 === '6 months' ? "bg-[#b18af8] text-white" : "" }`} value="6 months">6 months</ToggleGroup.Item>
-                        <ToggleGroup.Item className={`flex-grow p-6 rounded-r-lg mt-10 mb-10 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio1 === '12 months' ? "bg-[#b18af8] text-white" : "" }`} value="12 months">12 months</ToggleGroup.Item>
+                        <ToggleGroup.Item className={`flex-grow px-6 py-2 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio1 === '3 months' ? "bg-[#b18af8] text-white" : "" }`} value="3 months">3 months</ToggleGroup.Item>
+                        <ToggleGroup.Item className={`flex-grow px-6 py-2 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio1 === '6 months' ? "bg-[#b18af8] text-white" : "" }`} value="6 months">6 months</ToggleGroup.Item>
+                        <ToggleGroup.Item className={`flex-grow px-6 py-2 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio1 === '12 months' ? "bg-[#b18af8] text-white" : "" }`} value="12 months">12 months</ToggleGroup.Item>
                     </ToggleGroup.Root>
-                    <h2 className="pb-5">CONTRIBUTION FREQUENCY: </h2>
+                    <h2 className="py-5">CONTRIBUTION FREQUENCY: </h2>
                     <ToggleGroup.Root
                         className="flex"
                         type="single"
                         value={radio2}
-                        onValueChange={(radio2) => {
+                        onValueChange={(radio2: any) => {
                             if (radio2) setRadio2(radio2)
                         }}
                     >
-                        <ToggleGroup.Item className={`flex-grow p-6 rounded-l-lg border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio2 === '1 week' ? "bg-[#b18af8] text-white" : "" }`} value="1 week">1 week</ToggleGroup.Item>
-                        <ToggleGroup.Item className={`flex-grow p-6 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio2 === '2 weeks' ? "bg-[#b18af8] text-white" : "" }`} value="2 weeks">2 weeks</ToggleGroup.Item>
-                        <ToggleGroup.Item className={`flex-grow p-6 rounded-r-lg border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:opacity-70 ${radio2 === '1 month' ? "bg-[#b18af8] text-white" : "" }`} value="1 month">1 month</ToggleGroup.Item>
+                        <ToggleGroup.Item className={`flex-grow px-6 py-2 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio2 === '1 week' ? "bg-[#b18af8] text-white" : "" }`} value="1 week">1 week</ToggleGroup.Item>
+                        <ToggleGroup.Item className={`flex-grow px-6 py-2 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio2 === '2 weeks' ? "bg-[#b18af8] text-white" : "" }`} value="2 weeks">2 weeks</ToggleGroup.Item>
+                        <ToggleGroup.Item className={`flex-grow px-6 py-2 border-black border-2 bg-[#E5FFF8] text-[#372D40] hover:bg-[#b18af8] hover:opacity-70 ${radio2 === '1 month' ? "bg-[#b18af8] text-white" : "" }`} value="1 month">1 month</ToggleGroup.Item>
                     </ToggleGroup.Root>
-                    <p className="pt-20 text-[#372D40]">Your goal is to save ${title} in {radio1}</p>
-                    <button className="p-6 rounded-full m-32 bg-[#b18af8] text-white">Set Goal</button>
+                    <p className="py-5 text-center text-[#372D40]">Your goal is to save ${title} in the next {radio1}</p>
+                    <button className="mx-24 py-5 rounded-full bg-[#b18af8] text-white">START GROWING!</button>
                 </form>
             </div>
         </div>
