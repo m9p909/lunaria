0d95ca4630cdd73f7c694a0c5ff0237547798cb5
Jack Clarke <jackfulcher09@gmail.com>
2023-01-22 05:28:19 -0500

    flower progress

--- src/components/FlowerProgress.tsx
+++ src/components/FlowerProgress.tsx
@@ -1,13 +1,48 @@
 import React from "react";
+import Image from "next/image";
 
 interface FlowerProgress {
-    progress: number;
+    progress: 0 | 1 | 2 | 3 | 4 | 5
 }
 
-export default function FlowerProgress() {
+const FlowerImage = ({progress}: FlowerProgress) => {
+    return <Image alt={`stage ${progress} image`} height={200} width={200} src={`/assets/stage${progress}.PNG`}/>
+}
+
+export enum FlowerState {
+    Normal,
+    Happy,
+    Sad,
+    Dead
+}
+
+interface FlowerProps {
+    progress: 0 | 1 | 2 | 3 | 4 | 5
+    state: FlowerState
+}
+
+export default function FlowerProgress(props: FlowerProps) {
+    const normal = ""
+    const sad = "filter sepia"
+    const dead = "filter grayscale"
+    const happy = "brightness-150"
+    let className = normal;
+    switch(props.state){
+        case FlowerState.Happy:
+            className = happy;
+            break;
+        case FlowerState.Dead:
+            className = dead;
+            break;
+        case FlowerState.Sad:
+            className = sad;
+            break;
+        default:
+            break;
+    }
     return(
-        <section>
-            
+        <section className={className} >
+            <FlowerImage progress={props.progress}/>
         </section>
     )
 }
